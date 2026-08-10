1bd11ead0fdb603163a7f346fa676cd79f167c90
Kirredgard <kirredgard90@gmail.com>
2026-08-10 15:35:22 +0200

    env.js

--- env.js
+++ env.js
@@ -2,11 +2,12 @@
 // Ce fichier est chargé avant les modules V21.
 // En production, gardez Firebase sécurisé par Firestore Rules + domaines autorisés.
 window.__GP_FIREBASE_CONFIG__ = {
-  apiKey: "AIzaSyCBMi5IZrTMkaziOdqdnHdvKPqcpyOXqiM",
-  authDomain: "geniusproperty-50bfb.firebaseapp.com",
-  projectId: "geniusproperty-50bfb",
-  storageBucket: "geniusproperty-50bfb.firebasestorage.app",
-  messagingSenderId: "137061315421",
-  appId: "1:137061315421:web:d921da479b90436d52eeb4",
-  measurementId: "G-YRSG0YZJ46"
+    apiKey: "AIzaSyADAFpFf7vJuvfMBV-0Kr-h4k5NNNEW0Sk",
+  authDomain: "genius-property-test.firebaseapp.com",
+  projectId: "genius-property-test",
+  storageBucket: "genius-property-test.firebasestorage.app",
+  messagingSenderId: "165939850015",
+  appId: "1:165939850015:web:1b1d3197d3ad9119a01e2a",
+  measurementId: "G-DT7STVSZJW"
+
 };
